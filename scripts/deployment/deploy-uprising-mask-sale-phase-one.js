54ab9c4c17aa1309b2fd18cd9a5ee119f0f9cf63
Michael McClenaghan <michael@districtrace.com>
2022-07-23 13:53:10 +1000

    feature: update sale contract deployment script to add setup steps

--- scripts/deployment/deploy-uprising-mask-sale-phase-one.js
+++ scripts/deployment/deploy-uprising-mask-sale-phase-one.js
@@ -13,22 +13,30 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  console.log('attaching invite contract')
+  const env = 'PROD' // 'DEV' || 'PROD'
 
-  const DEFYUprisingInvite = await ethers.getContractFactory("DEFYUprisingInvite");
-  const defyUprisingInvite = DEFYUprisingInvite.attach('0xfc6a13353bf45462e304218ea51acd72da6430c4');
+  const addresses = {
+	inviteTier1: env === 'DEV' ? '0xFc6A13353Bf45462e304218EA51ACd72Da6430c4' : '0xa3b7945a9a964e6a8434c2dfa249181a818a5cd2',
+	inviteTier2: env === 'DEV' ? '0xc8Aa0FE090b17CcF594C31FFC314844eE625e900' : '0x9162c5dcD344B9B3C2527A77a8C2cd7F1334b6e7',
+	uprisingMask: env === 'DEV' ? '0x079C888558a553de2aC6D10d7877fEc5a63297b3' : '0x0973f5e8A888f3172c056099EB053879dE972684',
+  }
+
+  // We get the contract to deploy
+  console.log('Attaching invite contracts')
+
+  const DEFYUprisingInviteTier1 = await ethers.getContractFactory("DEFYUprisingInvite");
+  const defyUprisingInviteTier1 = DEFYUprisingInviteTier1.attach(addresses.inviteTier1);
   
   const DEFYUprisingInviteTier2 = await ethers.getContractFactory("DEFYUprisingInvite");
-  const defyUprisingInviteTier2 = DEFYUprisingInviteTier2.attach('0xc8Aa0FE090b17CcF594C31FFC314844eE625e900');
+  const defyUprisingInviteTier2 = DEFYUprisingInviteTier2.attach(addresses.inviteTier2);
 
-  console.log('attaching mask contract')
+  console.log('Attaching mask contract')
 
   const DEFYUprisingMask = await ethers.getContractFactory("DEFYUprisingMask");
-  const defyUprisingMask = DEFYUprisingMask.attach('0x5Dca8B930BF6dC68eDCdF5cc66e9657FD9593c49');
+  const defyUprisingMask = DEFYUprisingMask.attach(addresses.uprisingMask);
 
   const DEFYUprisingSalePhaseOne = await ethers.getContractFactory("DEFYUprisingSalePhaseOne");
-  const defyUprisingSalePhaseOne = await DEFYUprisingSalePhaseOne.deploy('0xfc6a13353bf45462e304218ea51acd72da6430c4', '0xc8Aa0FE090b17CcF594C31FFC314844eE625e900', '0x5Dca8B930BF6dC68eDCdF5cc66e9657FD9593c49');
+  const defyUprisingSalePhaseOne = await DEFYUprisingSalePhaseOne.deploy(addresses.inviteTier1, addresses.inviteTier2, addresses.uprisingMask);
 
   console.log('Deploying defyUprisingSalePhaseOne')
 
@@ -37,17 +45,19 @@ async function main() {
   console.log("defyUprisingSalePhaseOne deployed to:", defyUprisingSalePhaseOne.address);
 
   console.log('granting INVITE_SPENDER role on invite contract')
-  await defyUprisingInvite.grantRole('0xdccc1989d74912b93bf418567a92dfdbc8fde7ffc7bffb3218a0397a2c4285ae', defyUprisingSalePhaseOne.address);
+  await defyUprisingInviteTier1.grantRole('0xdccc1989d74912b93bf418567a92dfdbc8fde7ffc7bffb3218a0397a2c4285ae', defyUprisingSalePhaseOne.address);
   await defyUprisingInviteTier2.grantRole('0xdccc1989d74912b93bf418567a92dfdbc8fde7ffc7bffb3218a0397a2c4285ae', defyUprisingSalePhaseOne.address);
 
   console.log('granting MINTER_ROLE role on mask contract')
   await defyUprisingMask.grantRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', defyUprisingSalePhaseOne.address);
 
-  console.log('updating mint price on sale contract')
-  await defyUprisingSalePhaseOne.updateMintPrice(40000000000000000n);
-
-  console.log('updating tier 1 mint active')
-  await defyUprisingSalePhaseOne.updateTier1MintActive(true);
+  if (env === 'DEV') {
+	console.log('updating mint price on sale contract')
+	await defyUprisingSalePhaseOne.updateMintPrice(40000000000000000n);
+  
+	console.log('updating tier 1 mint active')
+	await defyUprisingSalePhaseOne.updateTier1MintActive(true);
+  }
 
   console.log("defyUprisingSalePhaseOne deployed to:", defyUprisingSalePhaseOne.address);
 }
